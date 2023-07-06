20d02ac1fabd652e008a513056afd53360a0ff77
philkoerber <philipp.koerber.93@gmail.com>
2023-07-06 10:55:49 +0900

    add browse route

--- app/Header.jsx
+++ app/Header.jsx
@@ -3,25 +3,31 @@ import React from 'react';
 function Header() {
     return (
         <header className="bg-gray-900 py-4">
-            <nav className="flex items-center justify-between px-4">
+            <nav className="flex items-center justify-between px-8">
                 <div>
                     <h1 className="text-white text-2xl font-bold">Chess Web App</h1>
                 </div>
-                <div className="space-x-4">
+                <div className="space-x-8">
                     <a
-                        href="#"
-                        className="text-white hover:text-gray-300"
-                    >
-                        Browse Games
-                    </a>
-                    <a
-                        href="#"
+                        href="/"
                         className="text-white hover:text-gray-300"
                     >
                         Home
                     </a>
                     <a
-                        href="#"
+                        href="/browse"
+                        className="text-white hover:text-gray-300"
+                    >
+                        Browse
+                    </a>
+                    <a
+                        href="/mygames"
+                        className="text-white hover:text-gray-300"
+                    >
+                        My Games
+                    </a>
+                    <a
+                        href="/profile"
                         className="text-white hover:text-gray-300"
                     >
                         Profile
---
+++ app/browse/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function Browse(props) {
+    return (
+        <div>
+            What are you looking for?
+        </div>
+    );
+}
+
+export default Browse;
