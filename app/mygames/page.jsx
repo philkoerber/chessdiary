11b22229547adb2f83157b82aec8cf1c705a8dca
philkoerber <philipp.koerber.93@gmail.com>
2023-07-10 15:25:44 +0900

    uninstall @bity/oauth2-auth-code-pkce

--- app/mygames/page.jsx
+++ app/mygames/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function MyGames(props) {
     return (
         <div>
-            myGames
+            myGames...
         </div>
     );
 }
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,40 +1,40 @@
-import React from 'react';
-import { AccessContext, HttpClient, OAuth2AuthCodePKCE } from '@bity/oauth2-auth-code-pkce';
+"use client"
 
-class Ctrl {
-    oauth = new OAuth2AuthCodePKCE({
-        authorizationUrl: "https://lichess.org/oauth",
-        tokenUrl: "https://lichess.org/api/token",
-        clientId: "example.com",
-        scopes: ["email:read"],
-        redirectUrl: "/myGames"
+import React, { useEffect } from 'react';
+import { OAuth2AuthCodePKCE } from '@bity/oauth2-auth-code-pkce';
 
+const oauth = new OAuth2AuthCodePKCE({
+  authorizationUrl: 'https://lichess.org/oauth',
+  tokenUrl: 'https://lichess.org/api/token',
+  clientId: 'YOUR_CLIENT_ID', // Replace with your actual client ID
+  scopes: ['email:read'],
+  redirectUrl: 'http://localhost:3000/myGames', // Replace with your redirect URL
+});
 
-    })
-}
-
-const getMyAccount = async () => {
-  try {
-        const response = await fetch(
-          'https://lichess.org/api/account'
-        );
-        const data = await response.json();
-    return (
-      data
-        );
+const Profile = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const code = await oauth.fetchAuthorizationCode();
+        // Exchange the authorization code for an access token
+        const token = await oauth.fetchToken(code);
+        // Make authenticated requests using the access token
+        const accountResponse = await fetch('https://lichess.org/api/account', {
+          headers: {
+            Authorization: `Bearer ${token.accessToken}`,
+          },
+        });
+        const accountData = await accountResponse.json();
+        console.log(accountData);
       } catch (error) {
-        console.log('Error fetching games data:', error);
+        console.log('Error fetching account data:', error);
       }
-}
+    };
 
-async function Profile(props) {
-    const account = await getMyAccount();
-    console.log(account)
-    return (
-        <div>
-            Login here
-        </div>
-    );
-}
+    fetchData();
+  }, []);
+
+  return <div className="w-full">Login here</div>;
+};
 
 export default Profile;
