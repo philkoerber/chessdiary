f103e07b16bb74450fc0f596084a2c336a549354
philkoerber <philipp.koerber.93@gmail.com>
2023-07-10 14:54:16 +0900

    add darker bg-color for smoother dev experience...

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,8 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-         <Header />
+      <body className="bg-gray-500">
+        <Header />
+        
         {children}
       </body>
     </html>
